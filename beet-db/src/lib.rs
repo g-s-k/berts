fe059151dc1da1b95ce0f2a3a725b8752dfb94fa
George Kaplan <george@georgekaplan.xyz>
2019-01-30 09:30:11 -0500

    add macro to make sqlite slightly less painful

--- beet-db/src/lib.rs
+++ beet-db/src/lib.rs
@@ -1,112 +1,163 @@
-use std::path::PathBuf;
+macro_rules! def_sqlite_struct {
+    ( $name:ident [ $( $field:ident: $typ:ty, )* ] ) => {
+        #[derive(Debug)]
+        pub struct $name {
+            $( pub $field: $typ ),*
+        }
+
+        impl $name {
+            pub fn from_row(db_row__: &::rusqlite::Row) -> Self {
+                let mut field_idx__ = 0;
+
+                $(
+                    let $field = db_row__.get(field_idx__);
+                    field_idx__ += 1;
+                )*
+
+                drop(field_idx__);
+
+                Self {
+                    $( $field ),*
+                }
+            }
+        }
+    };
+    ( $name:ident $table:ident $fields:tt ) => {
+        def_sqlite_struct! {
+            $name $fields
+        }
+
+        impl $name {
+            pub fn read_all(c: ::rusqlite::Connection) ->
+                ::std::result::Result<::std::vec::Vec<Self>, ::rusqlite::Error>
+            {
+                let mut stmt = c.prepare(concat!("SELECT * FROM ", stringify!($table)))?;
+                let rows = stmt.query_map(::rusqlite::NO_PARAMS, Self::from_row)?;
+
+                let mut v = ::std::vec::Vec::new();
+                for row in rows {
+                    v.push(row?);
+                }
+
+                Ok(v)
+            }
+        }
+    };
+}
 
 // Is this needed?
-pub struct Attribute {
-    pub id: usize,
-    pub entity_id: usize,
-    pub key: String,
-    pub value: String,
+def_sqlite_struct! {
+    Attribute [
+        id: u32,
+        entity_id: u32,
+        key: String,
+        value: String,
+    ]
 }
 
-pub struct Album {
-    pub id: usize,
-    pub artpath: PathBuf,
-    pub added: f32,
-    pub albumartist: String,
-    pub albumartist_sort: String,
-    pub albumartist_credit: String,
-    pub album: String,
-    pub genre: String,
-    pub year: u16,
-    pub month: u8,
-    pub day: u8,
-    pub disctotal: usize,
-    pub comp: bool,
-    pub mb_albumid: String,
-    pub mb_albumartistid: String,
-    pub mb_releasegroupid: String,
-    pub albumtype: String,
-    pub label: String,
-    pub asin: String,
-    pub catalognum: String,
-    pub script: String,
-    pub language: String,
-    pub country: String,
-    pub albumstatus: String,
-    pub albumdisambig: String,
-    pub rg_album_gain: f32,
-    pub rg_album_peak: f32,
-    pub r128_album_gain: usize,
-    pub original_year: u16,
-    pub original_month: u8,
-    pub original_day: u8,
+def_sqlite_struct! {
+    Album albums [
+        id: u32,
+        artpath: String,
+        added: f64,
+        albumartist: String,
+        albumartist_sort: String,
+        albumartist_credit: String,
+        album: String,
+        genre: String,
+        year: u16,
+        month: u8,
+        day: u8,
+        disctotal: u32,
+        comp: bool,
+        mb_albumid: String,
+        mb_albumartistid: String,
+        albumtype: String,
+        label: String,
+        mb_releasegroupid: String,
+        asin: String,
+        catalognum: String,
+        script: String,
+        language: String,
+        country: String,
+        albumstatus: String,
+        albumdisambig: String,
+        rg_album_gain: f64,
+        rg_album_peak: f64,
+        r128_album_gain: u32,
+        original_year: u16,
+        original_month: u8,
+        original_day: u8,
+ ]
 }
 
-pub struct Item {
-    pub id: usize,
-    pub path: PathBuf,
-    pub album_id: usize,
-    pub title: String,
-    pub artist: String,
-    pub artist_sort: String,
-    pub artist_credit: String,
-    pub album: String,
-    pub albumartist: String,
-    pub albumartist_sort: String,
-    pub albumartist_credit: String,
-    pub genre: String,
-    pub lyricist: String,
-    pub composer: String,
-    pub composer_sort: String,
-    pub arranger: String,
-    pub grouping: String,
-    pub year: u16,
-    pub month: usize,
-    pub day: usize,
-    pub track: usize,
-    pub tracktotal: usize,
-    pub disc: usize,
-    pub disctotal: usize,
-    pub lyrics: String,
-    pub comments: String,
-    pub bpm: usize,
-    pub comp: bool,
-    pub mb_trackid: String,
-    pub mb_albumid: String,
-    pub mb_artistid: String,
-    pub mb_albumartistid: String,
-    pub mb_releasetrackid: String,
-    pub mb_releasegroupid: String,
-    pub albumtype: String,
-    pub label: String,
-    pub acoustid_fingerprint: String,
-    pub acoustid_id: String,
-    pub asin: String,
-    pub catalognum: String,
-    pub script: String,
-    pub language: String,
-    pub country: String,
-    pub albumstatus: String,
-    pub media: String,
-    pub albumdisambig: String,
-    pub disctitle: String,
-    pub encoder: String,
-    pub rg_track_gain: f32,
-    pub rg_track_peak: f32,
-    pub rg_album_gain: f32,
-    pub rg_album_peak: f32,
-    pub r128_track_gain: usize,
-    pub r128_album_gain: usize,
-    pub original_year: u16,
-    pub original_month: u8,
-    pub original_day: u8,
-    pub initial_key: String,
-    pub length: f32,
-    pub bitrate: usize,
-    pub format: String,
-    pub samplerate: u16,
-    pub bitdepth: u16,
-    pub channels: u8,
-    pub mtime: f32,
-    pub added: f32,
+def_sqlite_struct! {
+    Item items [
+        id: u32,
+        path: String,
+        album_id: u32,
+        title: String,
+        artist: String,
+        artist_sort: String,
+        artist_credit: String,
+        album: String,
+        albumartist: String,
+        albumartist_sort: String,
+        albumartist_credit: String,
+        genre: String,
+        lyricist: String,
+        composer: String,
+        composer_sort: String,
+        arranger: String,
+        grouping: String,
+        year: u16,
+        month: u32,
+        day: u32,
+        track: u32,
+        tracktotal: u32,
+        disc: u32,
+        disctotal: u32,
+        lyrics: String,
+        comments: String,
+        bpm: u32,
+        comp: bool,
+        mb_trackid: String,
+        mb_albumid: String,
+        mb_artistid: String,
+        mb_albumartistid: String,
+        mb_releasetrackid: String,
+        albumtype: String,
+        label: String,
+        acoustid_fingerprint: String,
+        acoustid_id: String,
+        mb_releasegroupid: String,
+        asin: String,
+        catalognum: String,
+        script: String,
+        language: String,
+        country: String,
+        albumstatus: String,
+        media: String,
+        albumdisambig: String,
+        disctitle: String,
+        encoder: String,
+        rg_track_gain: f64,
+        rg_track_peak: f64,
+        rg_album_gain: f64,
+        rg_album_peak: f64,
+        r128_track_gain: u32,
+        r128_album_gain: u32,
+        original_year: u16,
+        original_month: u8,
+        original_day: u8,
+        initial_key: String,
+        length: f64,
+        bitrate: u32,
+        format: String,
+        samplerate: u16,
+        bitdepth: u16,
+        channels: u8,
+        mtime: f64,
+        added: f64,
+    ]
 }
