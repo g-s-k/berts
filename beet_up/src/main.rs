801d6f297ff1ef9a7d61df53e43c262f65d7924c
George Kaplan <george@georgekaplan.xyz>
2019-01-31 06:47:03 -0500

    mock the api with warp

--- beet_up/src/main.rs
+++ beet_up/src/main.rs
@@ -1,7 +1,8 @@
-use std::net::IpAddr;
+use std::net::{IpAddr, SocketAddr};
 use std::path::PathBuf;
 
 use structopt::StructOpt;
+use warp::{path, Filter, Rejection, Reply};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "beet-up")]
@@ -14,7 +15,7 @@ struct Cli {
     host: IpAddr,
     /// The port to listen on.
     #[structopt(short, long, default_value = "8337")]
-    port: usize,
+    port: u16,
     /// The CORS allowed origin. CORS is off if not provided.
     #[structopt(long)]
     cors: Option<String>,
@@ -32,7 +33,37 @@ struct Cli {
     db_path: PathBuf,
 }
 
+fn router() -> impl Filter<Extract = impl Reply, Error = Rejection> {
+    let get_all = path::end().map(|| "get all");
+    let get_by_id = path::param().map(|id: u32| format!("get one id: {}", id));
+    let get_by_ids = path::param().map(|ids: String| format!("get these ids: {}", ids));
+    let get_by_path = path("path")
+        .and(path::tail())
+        .map(|t: path::Tail| format!("get this path: {:#?}", PathBuf::from(t.as_str())));
+    let get_by_query = path("query")
+        .and(path::param())
+        .map(|q: String| format!("get the results of this query: {:?}", q));
+
+    // TODO: /item/:id/file
+    let item = path("item").and(
+        get_all
+            .or(get_by_id)
+            .or(get_by_path)
+            .or(get_by_query)
+            .or(get_by_ids),
+    );
+
+    let album = path("album").and(get_all.or(get_by_id).or(get_by_query).or(get_by_ids));
+
+    let stats = path("stats").map(|| "library stats");
+
+    item.or(album).or(stats)
+}
+
 fn main() {
     let cli = Cli::from_args();
-    println!("Now serving at {}:{}.", cli.host, cli.port);
+    let addr = SocketAddr::new(cli.host, cli.port);
+    println!("Now listening at http://{}.", addr);
+
+    warp::serve(router()).run(addr)
 }
