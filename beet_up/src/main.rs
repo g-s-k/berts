b2d24a2843af2aae56ef2253f08b43a7e9e2bd1d
George Kaplan <george@georgekaplan.xyz>
2019-01-30 22:23:39 -0500

    reorganized package, add fancy structopt

---
+++ beet_up/src/main.rs
@@ -0,0 +1,38 @@
+use std::net::IpAddr;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "beet-up")]
+#[structopt(about = "a web player for beets")]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+#[structopt(rename_all = "kebab-case")]
+struct Cli {
+    /// The server hostname.
+    #[structopt(long, parse(try_from_str), default_value = "0.0.0.0")]
+    host: IpAddr,
+    /// The port to listen on.
+    #[structopt(short, long, default_value = "8337")]
+    port: usize,
+    /// The CORS allowed origin. CORS is off if not provided.
+    #[structopt(long)]
+    cors: Option<String>,
+    /// Support credentials when using CORS.
+    #[structopt(long, requires = "cors")]
+    cors_supports_credentials: bool,
+    /// Respect forwarded headers when behind a reverse proxy.
+    #[structopt(long)]
+    reverse_proxy: bool,
+    /// Include paths in item responses.
+    #[structopt(long)]
+    include_paths: bool,
+    /// Path to your beet database.
+    #[structopt(parse(from_os_str))]
+    db_path: PathBuf,
+}
+
+fn main() {
+    let cli = Cli::from_args();
+    println!("Now serving at {}:{}.", cli.host, cli.port);
+}
