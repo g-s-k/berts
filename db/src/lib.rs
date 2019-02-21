0db3d6e438030db477cde5db715eb70a4fe5e600
George Kaplan <george.kaplan@visionistinc.com>
2019-02-21 18:19:11 -0500

    get fetching to work in the client

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -7,7 +7,9 @@ extern crate serde_derive;
 
 use std::path::PathBuf;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub use rusqlite::Error;
+#[cfg(not(target_arch = "wasm32"))]
 use rusqlite::{Connection, OpenFlags};
 
 mod tests;
@@ -16,11 +18,12 @@ macro_rules! def_sqlite_struct {
     ( $(#[$outer:meta])* $name:ident [ $( $(#[$inner:meta])* $field:ident: $typ:ty $(; $func:ident)?, )* ]
     ) => {
         $(#[$outer])*
-        #[derive(Clone, Debug, Serialize)]
+        #[derive(Clone, Debug, Deserialize, Serialize)]
         pub struct $name {
             $( $(#[$inner])* pub $field: $typ ),*
         }
 
+        #[cfg(not(target_arch = "wasm32"))]
         impl $name {
             #[allow(unused_assignments)]
             /// Bind the metadata for a single entry.
@@ -51,6 +54,7 @@ macro_rules! def_sqlite_struct {
     };
 
     ( $name:ident $table:expr ) => {
+        #[cfg(not(target_arch = "wasm32"))]
         impl $name {
             #[doc = "Bind each of the entries in the `"]
             #[doc = $table]
@@ -114,57 +118,57 @@ def_sqlite_struct! {
         #[serde(skip)]
         added: f64,
         albumartist: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumartist_sort: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumartist_credit: String,
         album: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         genre: String,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         year: u16,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         month: u8,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         day: u8,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         disctotal: u32,
         comp: bool,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_albumid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_albumartistid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumtype: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         label: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_releasegroupid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         asin: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         catalognum: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         script: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         language: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         country: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumstatus: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumdisambig: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         rg_album_gain: Option<f64>,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         rg_album_peak: Option<f64>,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         r128_album_gain: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         original_year: u16,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         original_month: u8,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         original_day: u8,
  ]
 }
@@ -176,133 +180,134 @@ def_sqlite_struct! {
         /// This is converted lossily - any invalid UTF-8 will be
         /// [transcribed as the replacement character.](https://doc.rust-lang.org/std/string/struct.String.html#method.from_utf8_lossy)
         path: PathBuf; blob_to_path,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         album_id: Option<u32>,
         title: String,
         artist: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         artist_sort: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         artist_credit: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         album: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumartist: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumartist_sort: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumartist_credit: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         genre: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         lyricist: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         composer: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         composer_sort: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         arranger: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         grouping: String,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         year: u16,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         month: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         day: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         track: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         tracktotal: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         disc: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         disctotal: u32,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         lyrics: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         comments: String,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         bpm: u32,
         comp: bool,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_trackid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_albumid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_artistid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_albumartistid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_releasetrackid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumtype: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         label: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         acoustid_fingerprint: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         acoustid_id: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         mb_releasegroupid: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         asin: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         catalognum: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         script: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         language: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         country: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumstatus: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         media: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         albumdisambig: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         disctitle: String,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         encoder: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         rg_track_gain: Option<f64>,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         rg_track_peak: Option<f64>,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         rg_album_gain: Option<f64>,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         rg_album_peak: Option<f64>,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         r128_track_gain: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         r128_album_gain: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         original_year: u16,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         original_month: u8,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         original_day: u8,
-        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde(skip_serializing_if = "Option::is_none", default)]
         initial_key: Option<String>,
         length: f64,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         bitrate: u32,
-        #[serde(skip_serializing_if = "String::is_empty")]
+        #[serde(skip_serializing_if = "String::is_empty", default)]
         format: String,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         samplerate: u32,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         bitdepth: u16,
-        #[serde(skip_serializing_if = "is_num_zero")]
+        #[serde(skip_serializing_if = "is_num_zero", default)]
         channels: u8,
-        #[serde(skip)]
+        #[serde(skip, default)]
         mtime: f64,
-        #[serde(skip)]
+        #[serde(skip, default)]
         added: f64,
     ]
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 pub fn read_all(db_path: PathBuf) -> Result<(Vec<Album>, Vec<Item>), Error> {
     let conn = Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_ONLY)?;
     Ok((Album::read_all(&conn)?, Item::read_all(&conn)?))
--- up/www/src/app/mod.rs
+++ up/www/src/app/mod.rs
@@ -1,22 +1,48 @@
-use stdweb::{_js_impl, js};
+use failure::Error;
+use stdweb::{__internal_console_unsafe, _js_impl, console, js};
+use yew::format::{Json, Nothing};
 use yew::prelude::*;
+use yew::services::{
+    fetch::{FetchService, FetchTask, Request, Response},
+    Task,
+};
+
+use beet_db::{Album, Item};
 
 pub enum Msg {
     Input(String),
     Clear,
+    RequestFailed,
+    FetchAlbums,
+    AlbumsFetched(Result<Vec<Album>, Error>),
+    FetchItems,
+    ItemsFetched(Result<Vec<Item>, Error>),
 }
 
 pub struct App {
+    link: ComponentLink<App>,
+    fetch_service: FetchService,
+    fetch_tasks: Vec<FetchTask>,
     query: String,
+    albums: Vec<Album>,
+    items: Vec<Item>,
 }
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+    fn create(_: Self::Properties, mut link: ComponentLink<Self>) -> Self {
+        link.send_self(Msg::FetchAlbums);
+        link.send_self(Msg::FetchItems);
+
         Self {
+            link,
+            fetch_service: FetchService::new(),
+            fetch_tasks: Vec::new(),
             query: String::new(),
+            albums: Vec::new(),
+            items: Vec::new(),
         }
     }
 
@@ -26,7 +52,44 @@ impl Component for App {
                 self.query.clear();
                 js! { document.getElementById("searchbar").focus() }
             }
-            Msg::Input(s) => self.query = s,
+            Msg::Input(s) => {
+                self.query = s;
+            }
+            Msg::RequestFailed => self.prune_fetches(),
+            Msg::FetchAlbums => {
+                let req = Request::get("http://localhost:8337/album")
+                    .body(Nothing)
+                    .unwrap();
+                let task = self
+                    .fetch_service
+                    .fetch(req, self.link.send_back(album_fetch_cback));
+                self.fetch_tasks.push(task);
+            }
+            Msg::AlbumsFetched(Ok(a)) => {
+                self.albums = a;
+                self.prune_fetches();
+            }
+            Msg::AlbumsFetched(Err(e)) => {
+                console!(error, format!("Fetch error: {:#?}", e));
+                self.prune_fetches();
+            }
+            Msg::FetchItems => {
+                let req = Request::get("http://localhost:8337/item")
+                    .body(Nothing)
+                    .unwrap();
+                let task = self
+                    .fetch_service
+                    .fetch(req, self.link.send_back(item_fetch_cback));
+                self.fetch_tasks.push(task);
+            }
+            Msg::ItemsFetched(Ok(a)) => {
+                self.items = a;
+                self.prune_fetches();
+            }
+            Msg::ItemsFetched(Err(e)) => {
+                console!(error, format!("Fetch error: {:#?}", e));
+                self.prune_fetches();
+            }
         }
 
         true
@@ -35,27 +98,76 @@ impl Component for App {
 
 impl Renderable<App> for App {
     fn view(&self) -> Html<Self> {
+        let track_list = self.items.iter().map(|item| {
+            html! {
+                <tr>
+                    <td>{ &item.title }</td>
+                    <td>{ &item.artist }</td>
+                    <td>{ &item.album }</td>
+                </tr>
+            }
+        });
+
+        let filter_list = if self.query.is_empty() {
+            html! { <div class="EmptyFilterList", >{ "No filter applied" }</div> }
+        } else {
+            html! { <ul class="FilterList", >
+                     </ul> }
+        };
+
         html! {
             <div class="SplitPane", >
                 <div class="SideNav", >
-                <div class="input", >
-                <input
-                    id="searchbar",
-                    placeholder="Enter query...",
-                    oninput=|e| Msg::Input(e.value),
-                    value=&self.query,
-                />
-                <i onclick=|_| Msg::Clear, >{ "×" }</i>
-                </div>
+                    <div class="input", >
+                        <input
+                            id="searchbar",
+                            placeholder="Enter query...",
+                            oninput=|e| Msg::Input(e.value),
+                            value=&self.query,
+                        />
+                        <i onclick=|_| Msg::Clear, >{ "×" }</i>
+                    </div>
+                { filter_list }
                 </div>
                 <div class="Collection", >
                     <div class="ArtView", >
                     </div>
                     <div class="PaneDivider", />
                     <div class="TrackList", >
+                        <table>
+                            <tbody>
+                                { for track_list }
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         }
+    }
+}
+
+impl App {
+    fn prune_fetches(&mut self) {
+        self.fetch_tasks.retain(Task::is_active);
+    }
+}
+
+fn album_fetch_cback(response: Response<Json<Result<Vec<Album>, Error>>>) -> Msg {
+    let (meta, Json(data)) = response.into_parts();
+
+    if meta.status.is_success() {
+        Msg::AlbumsFetched(data)
+    } else {
+        Msg::RequestFailed
+    }
+}
+
+fn item_fetch_cback(response: Response<Json<Result<Vec<Item>, Error>>>) -> Msg {
+    let (meta, Json(data)) = response.into_parts();
+
+    if meta.status.is_success() {
+        Msg::ItemsFetched(data)
+    } else {
+        Msg::RequestFailed
     }
 }
